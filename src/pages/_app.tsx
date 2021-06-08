55c6d44f6c267cb44151f07a7df277b0157dcca7
hakushun <hakushun.pianist@gmail.com>
2021-06-08 18:11:41 +0900

    feat: _app.tsxにLayoutコンポーネント実装, overlay用のDOM追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { AppProps } from 'next/app';
 import { wrapper } from '../redux/store';
+import { Layout } from '../components/Layout';
 
 import '../styles/reset.scss';
 import '../styles/global.scss';
 
 function App({ Component, pageProps }: AppProps): React.ReactElement {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+      <div id="overlay" />
+    </>
+  );
 }
 
 export default wrapper.withRedux(App);
