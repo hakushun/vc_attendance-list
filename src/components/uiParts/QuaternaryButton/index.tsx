4cca5433e3a6326627bc1bc41bef79209586d7df
hakushun <hakushun.pianist@gmail.com>
2021-06-08 20:27:39 +0900

    feat: QuaternaryButtonのProps修正

--- src/components/uiParts/QuaternaryButton/index.tsx
+++ src/components/uiParts/QuaternaryButton/index.tsx
@@ -3,10 +3,11 @@ import styles from './index.module.scss';
 
 type Props = {
   label: string;
+  handleClick: () => void;
 };
-export const QuaternaryButton: React.VFC<Props> = ({ label }) => {
+export const QuaternaryButton: React.VFC<Props> = ({ label, handleClick }) => {
   return (
-    <button type="button" className={styles.root}>
+    <button type="button" className={styles.root} onClick={handleClick}>
       {label}
     </button>
   );
