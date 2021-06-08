a2583dc98d4713c28c6a78b76a240114c1898b5c
hakushun <hakushun.pianist@gmail.com>
2021-06-08 20:26:49 +0900

    refactor: firebaseAuth.tsの関数名変更, mapUserDataの接続

--- src/libs/firebase/firebaseAuth.ts
+++ src/libs/firebase/firebaseAuth.ts
@@ -1,32 +1,36 @@
 import firebase from 'firebase/app';
+import { AuthPayload, Userdata } from '../../redux/modules/user';
+import { mapUserData } from '../utils/mapUserData';
 
-type AuthPayload = {
-  email: string;
-  password: string;
-};
-
-export const signUp = async ({ email, password }: AuthPayload): Promise<firebase.User | null> => {
+export const signUpWithFirebase = async ({ email, password }: AuthPayload): Promise<Userdata> => {
   try {
     const { user } = await firebase.auth().createUserWithEmailAndPassword(email, password);
-    return user;
-  } catch(err) {
+    if (!user) return null;
+    return mapUserData(user);
+  } catch (err) {
     // TODO: エラー処理追加
     alert(err);
     return null;
   }
 };
 
-export const signIn = async ({ email, password }: AuthPayload): Promise<firebase.User | null> => {
+export const signInWithFirebase = async ({ email, password }: AuthPayload): Promise<Userdata> => {
   try {
     const { user } = await firebase.auth().signInWithEmailAndPassword(email, password);
-    return user;
-  } catch(err) {
+    if (!user) return null;
+    return mapUserData(user);
+  } catch (err) {
     // TODO: エラー処理追加
     alert(err);
     return null;
   }
 };
 
-export const signOut = async (): Promise<void> => {
-  await firebase.auth().signOut();
+export const signOutWithFirebase = async (): Promise<void> => {
+  try {
+    await firebase.auth().signOut();
+  } catch (err) {
+    // TODO: エラー処理追加
+    alert(err);
+  }
 };
