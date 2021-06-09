6940068a4c2040e46986c6683ec8cb25d65d42fd
hakushun <hakushun.pianist@gmail.com>
2021-06-09 14:08:44 +0900

    feat: LinkButtonコンポーネント作成

---
+++ src/components/uiParts/LinkButton/index.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+import React from 'react';
+import styles from './index.module.scss';
+
+type Props = {
+  href: string;
+  label: string;
+};
+export const LinkButton: React.VFC<Props> = ({ href, label }) => {
+  return (
+    <Link href={href}>
+      <a className={styles.root}>{label}</a>
+    </Link>
+  );
+};
