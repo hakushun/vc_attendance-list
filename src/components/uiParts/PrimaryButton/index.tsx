9945e3e7d2bfc0122400b029ad82be68d36b9746
hakushun <hakushun.pianist@gmail.com>
2021-06-09 14:08:01 +0900

    feat: PrimaryButtonのPropsにtypeとhandleClickを追加

--- src/components/uiParts/PrimaryButton/index.tsx
+++ src/components/uiParts/PrimaryButton/index.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import styles from './index.module.scss';
 
 type Props = {
+  type: 'button' | 'submit';
   label: string;
+  handleClick: (_e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
 };
-export const PrimaryButton: React.VFC<Props> = ({ label }) => {
+export const PrimaryButton: React.VFC<Props> = ({ type, label, handleClick }) => {
   return (
-    <button type="button" className={styles.root}>
+    <button type={type} className={styles.root} onClick={handleClick}>
       {label}
     </button>
   );
