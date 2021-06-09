57dc20201dbb8a84c7d9076cddf4cefc2de29959
hakushun <hakushun.pianist@gmail.com>
2021-06-09 19:51:04 +0900

    feat: Badgeコンポーネント作成

---
+++ src/components/uiParts/Badge/index.tsx
@@ -0,0 +1,16 @@
+import clsx from 'clsx';
+import React from 'react';
+import styles from './index.module.scss';
+
+type Props = {
+  type: 'required' | 'optional';
+};
+export const Badge: React.VFC<Props> = ({ type }) => {
+  if (type === 'required') {
+    return <span className={clsx(styles.root, styles.required)}>必須</span>;
+  }
+  if (type === 'optional') {
+    return <span className={clsx(styles.root, styles.optional)}>任意</span>;
+  }
+  return null;
+};
