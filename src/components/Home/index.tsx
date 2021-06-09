99a06fdb254164e82dbef44c5031ae6f7ea83f99
hakushun <hakushun.pianist@gmail.com>
2021-06-09 14:10:04 +0900

    feat: Homeにuser認証中のLoadingアニメーション実装

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import { useUser } from '../../hooks/useUser';
+import { Loading } from '../uiParts/Loading';
 
 export const Home: React.VFC = () => {
+  const { isLoading } = useUser();
+
+  if(isLoading) return <Loading />
+
   return <section>HOME</section>;
 };
