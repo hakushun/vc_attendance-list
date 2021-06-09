45b40d20e61e92bb01f3c22a946ca1ea3f90ae9c
hakushun <hakushun.pianist@gmail.com>
2021-06-09 14:09:12 +0900

    refactor: HeaderにuseSign接続

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -1,32 +1,34 @@
 import React from 'react';
 import { useShow } from '../../../hooks/useShow';
+import { useSign } from '../../../hooks/useSign';
 import { useUser } from '../../../hooks/useUser';
 import { QuaternaryButton } from '../../uiParts/QuaternaryButton';
 import styles from './index.module.scss';
 
-export const Header: React.VFC = () => {
-  const { user, handleSignOut } = useUser();
+export const Header: React.VFC= () => {
+  const { user } = useUser();
+  const { handleSignOut } = useSign();
   const { handleToggleEventForm, handleToggleSetting } = useShow();
 
   return (
     <header className={styles.root}>
       <div className={styles.inner}>
         <h1 className={styles.title}>出欠さん</h1>
-        <nav className={styles.nav}>
-          <ul className={styles.list}>
-            <li>
-              <QuaternaryButton label="イベント作成" handleClick={handleToggleEventForm} />
-            </li>
-            <li>
-              <QuaternaryButton label="設定" handleClick={handleToggleSetting} />
-            </li>
-            {user && (
+        {user && (
+          <nav className={styles.nav}>
+            <ul className={styles.list}>
+              <li>
+                <QuaternaryButton label="イベント作成" handleClick={handleToggleEventForm} />
+              </li>
+              <li>
+                <QuaternaryButton label="設定" handleClick={handleToggleSetting} />
+              </li>
               <li>
                 <QuaternaryButton label="Logout" handleClick={handleSignOut} />
               </li>
-            )}
-          </ul>
-        </nav>
+            </ul>
+          </nav>
+        )}
       </div>
     </header>
   );
