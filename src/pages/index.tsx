964b0e2d8f063c5d89ff7af0d12071a71c6fc3db
hakushun <hakushun.pianist@gmail.com>
2021-06-09 14:10:25 +0900

    feat: rootにHomeコンポーネント接続

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
+import { Home } from '../components/Home';
 
-const index: React.VFC = () => <div>Home</div>;
+const index: React.VFC = () => <Home />;
 
 export default index;
