e3e4cfa546d568c54222264008177a77e8c68152
hakushun <hakushun.pianist@gmail.com>
2021-06-11 17:53:54 +0900

    feat: tab.ts実装

---
+++ src/redux/modules/tab.ts
@@ -0,0 +1,32 @@
+import { createSelector } from 'reselect';
+import actionCreatorFactory from 'typescript-fsa';
+import { reducerWithInitialState } from 'typescript-fsa-reducers';
+import { RootState } from './reducers';
+
+export type Value = 'practice' | 'program' | 'role' | 'part';
+export type Tab = {
+  setting: Value;
+};
+type ChangePayload = {
+  [s: string]: Value;
+};
+
+// action
+const actionCreator = actionCreatorFactory();
+
+export const change = actionCreator<ChangePayload>('CHANGE_TAB');
+
+// initial state
+const INITIAL_STATE: Tab = {
+  setting: 'practice',
+};
+
+// reducer
+const reducer = reducerWithInitialState(INITIAL_STATE).case(change, (state, payload) => ({
+  ...state,
+  ...payload,
+}));
+export default reducer;
+
+// selector
+export const selectTab = createSelector([(state: RootState) => state.ui.tab], (tab) => tab);
