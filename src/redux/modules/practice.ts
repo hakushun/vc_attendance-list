2f0efcc9b84908c8db24ff55f5d4a62f6585aba2
hakushun <hakushun.pianist@gmail.com>
2021-06-12 14:49:01 +0900

    feat: practice.ts実装

---
+++ src/redux/modules/practice.ts
@@ -0,0 +1,78 @@
+import { createSelector } from 'reselect';
+import actionCreatorFactory from 'typescript-fsa';
+import { reducerWithInitialState } from 'typescript-fsa-reducers';
+import { asyncFactory } from 'typescript-fsa-redux-thunk';
+import { Location } from './locations';
+import { Plan } from './plans';
+import { Remark } from './remarks';
+import { RootState } from './reducers';
+import { updatePractice } from '../../libs/firestore/crudPractice';
+import { Event } from './event';
+
+export type PracticeItem = {
+  locations: Location[];
+  plans: Plan[];
+  remarks: Remark[];
+};
+type Practice = {
+  practice: PracticeItem;
+  isLoading: boolean;
+};
+export type UpdatePayload = {
+  event: Event;
+  practice: PracticeItem;
+};
+interface CustomError extends Error {
+  name: string;
+  message: string;
+}
+// action
+const actionCreator = actionCreatorFactory();
+const asyncActionCreator = asyncFactory<Practice>(actionCreator);
+
+export const subscribePractice = actionCreator<PracticeItem>('SUBSCRIBE_PRACTICE');
+export const update = asyncActionCreator<UpdatePayload, void, CustomError>(
+  'UPDATE_PRACTICE',
+  async (payload) => {
+    await updatePractice(payload);
+  },
+);
+// initial state
+const INITIAL_STATE: Practice = {
+  practice: {
+    locations: [],
+    plans: [],
+    remarks: [],
+  },
+  isLoading: false,
+};
+
+// reducer
+const reducer = reducerWithInitialState(INITIAL_STATE)
+  .case(subscribePractice, (state, payload) => ({
+    ...state,
+    practice: { ...payload },
+  }))
+  .case(update.async.started, (state) => ({
+    ...state,
+    isLoading: true,
+  }))
+  .case(update.async.done, (state) => ({
+    ...state,
+    isLoading: false,
+  }))
+  .case(update.async.failed, (state) => ({
+    ...state,
+    isLoading: false,
+  }));
+export default reducer;
+
+// selector
+export const selectPractice = createSelector(
+  [(state: RootState) => state.domain.practice.practice],
+  (practice) => practice,
+);
+export const selectIsLoading = createSelector(
+  [(state: RootState) => state.domain.practice.isLoading],
+  (isLoading) => isLoading,
+);
