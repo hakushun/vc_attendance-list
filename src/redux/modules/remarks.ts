74d18490184b2564e44c891f9a1a4bfcfd6b8097
hakushun <hakushun.pianist@gmail.com>
2021-06-12 14:48:52 +0900

    feat: remarks.ts実装

---
+++ src/redux/modules/remarks.ts
@@ -0,0 +1,41 @@
+import { createSelector } from 'reselect';
+import actionCreatorFactory from 'typescript-fsa';
+import { reducerWithInitialState } from 'typescript-fsa-reducers';
+import { subscribePractice } from './practice';
+import { RootState } from './reducers';
+
+export type Remark = {
+  dateId: string;
+  content: string;
+};
+export type Remarks = {
+  remarks: Remark[];
+};
+type ChangePayload = Partial<Remark>;
+// action
+const actionCreator = actionCreatorFactory();
+
+export const changeRemark = actionCreator<ChangePayload>('CHANGE_REMARK');
+
+// initial state
+const INITIAL_STATE: Remarks = {
+  remarks: [],
+};
+
+// reducer
+const reducer = reducerWithInitialState(INITIAL_STATE)
+  .case(changeRemark, (state, payload) => ({
+    remarks: state.remarks.map((remark) =>
+      remark.dateId === payload.dateId ? { ...remark, ...payload } : remark,
+    ),
+  }))
+  .case(subscribePractice, (_state, payload) => ({
+    remarks: payload.remarks,
+  }));
+export default reducer;
+
+// selector
+export const selectRemarks = createSelector(
+  [(state: RootState) => state.app.remarks.remarks],
+  (remarks) => remarks,
+);
