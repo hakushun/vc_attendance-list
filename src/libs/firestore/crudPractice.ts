a12161a4488142a6bae5b4b369a4fa756dceccaf
hakushun <hakushun.pianist@gmail.com>
2021-06-12 14:47:32 +0900

    feat: crudPractice.ts実装

---
+++ src/libs/firestore/crudPractice.ts
@@ -0,0 +1,18 @@
+import { getInstance } from './getInstance';
+import { Event } from '../../redux/modules/event';
+import { UpdatePayload } from '../../redux/modules/practice';
+
+const db = getInstance();
+
+export const createPractice = async (event: Event): Promise<void> => {
+  const practice = {
+    locations: event.dates.map((date) => ({ eventId: event.id, dateId: date.id })),
+    plans: event.dates.map((date) => ({ eventId: event.id, dateId: date.id })),
+    remarks: event.dates.map((date) => ({ eventId: event.id, dateId: date.id })),
+  };
+  await db.collection('practices').doc(event.id).set(practice);
+};
+
+export const updatePractice = async ({ event, practice }: UpdatePayload): Promise<void> => {
+  await db.collection('practices').doc(event.id).set(practice, { merge: true });
+};
