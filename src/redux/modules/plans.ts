22f9cb5fa1f8b4305954cb1b6fc1c61df504e302
hakushun <hakushun.pianist@gmail.com>
2021-06-12 14:48:42 +0900

    feat: plans.ts実装

---
+++ src/redux/modules/plans.ts
@@ -0,0 +1,42 @@
+import { createSelector } from 'reselect';
+import actionCreatorFactory from 'typescript-fsa';
+import { reducerWithInitialState } from 'typescript-fsa-reducers';
+import { subscribePractice } from './practice';
+import { RootState } from './reducers';
+
+export type Plan = {
+  dateId: string;
+  category: string;
+  schedule: string;
+};
+export type Plans = {
+  plans: Plan[];
+};
+type ChangePayload = Partial<Plan>;
+// action
+const actionCreator = actionCreatorFactory();
+
+export const changePlan = actionCreator<ChangePayload>('CHANGE_PLAN');
+
+// initial state
+const INITIAL_STATE: Plans = {
+  plans: [],
+};
+
+// reducer
+const reducer = reducerWithInitialState(INITIAL_STATE)
+  .case(changePlan, (state, payload) => ({
+    plans: state.plans.map((plan) =>
+      plan.dateId === payload.dateId ? { ...plan, ...payload } : plan,
+    ),
+  }))
+  .case(subscribePractice, (_state, payload) => ({
+    plans: payload.plans,
+  }));
+export default reducer;
+
+// selector
+export const selectPlans = createSelector(
+  [(state: RootState) => state.app.plans.plans],
+  (plans) => plans,
+);
