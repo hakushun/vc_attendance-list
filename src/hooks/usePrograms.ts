b854123968ef7196d580b4887aa9ede4bc8ec681
hakushun <hakushun.pianist@gmail.com>
2021-06-14 14:55:57 +0900

    feat: usePrograms.ts実装

---
+++ src/hooks/usePrograms.ts
@@ -0,0 +1,46 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getInstance } from '../libs/firestore/getInstance';
+import { selectEvent } from '../redux/modules/event';
+import { Program, selectProgram } from '../redux/modules/program';
+import {
+  selectIsLoading,
+  selectPrograms,
+  subscribeProgram,
+  update,
+} from '../redux/modules/programs';
+
+type Hooks = {
+  programs: Program[];
+  isLoading: boolean;
+  handleUpdate: (_e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+};
+export const usePrograms = (eventId: string): Hooks => {
+  const db = getInstance();
+  const dispatch = useDispatch();
+  const event = useSelector(selectEvent);
+  const program = useSelector(selectProgram);
+  const programs = useSelector(selectPrograms);
+  const isLoading = useSelector(selectIsLoading);
+
+  const handleUpdate = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+    // TODO:validation
+    if (program.some((item) => item.name.trim() === '')) return;
+    dispatch(update({ event, program }));
+  };
+
+  useEffect(() => {
+    const unsubscribe = db
+      .collection('programs')
+      .doc(eventId)
+      .onSnapshot((snapshot) => {
+        const data = snapshot.data() as { program: Program[] } | undefined;
+        data && dispatch(subscribeProgram(data.program));
+      });
+    return () => unsubscribe();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [eventId]);
+
+  return { programs, isLoading, handleUpdate };
+};
