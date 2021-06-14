d4a4b1437c40fcae62e4eaad644c25f4ef245ce3
hakushun <hakushun.pianist@gmail.com>
2021-06-14 14:55:32 +0900

    feat: programs.ts実装

---
+++ src/redux/modules/programs.ts
@@ -0,0 +1,68 @@
+import { createSelector } from 'reselect';
+import actionCreatorFactory from 'typescript-fsa';
+import { reducerWithInitialState } from 'typescript-fsa-reducers';
+import { asyncFactory } from 'typescript-fsa-redux-thunk';
+import { RootState } from './reducers';
+import { Program } from './program';
+import { updateProgram } from '../../libs/firestore/crudProgram';
+import { Event } from './event';
+
+export type Programs = {
+  programs: Program[];
+  isLoading: boolean;
+};
+export type UpdatePayload = {
+  event: Event;
+  program: Program[];
+};
+interface CustomError extends Error {
+  name: string;
+  message: string;
+}
+// action
+const actionCreator = actionCreatorFactory();
+const asyncActionCreator = asyncFactory<Programs>(actionCreator);
+
+export const subscribeProgram = actionCreator<Program[]>('SUBSCRIBE_PROGRAM');
+export const update = asyncActionCreator<UpdatePayload, void, CustomError>(
+  'UPDATE_PROGRAM',
+  async (payload) => {
+    await updateProgram(payload);
+  },
+);
+
+// initial state
+const INITIAL_STATE: Programs = {
+  programs: [],
+  isLoading: false,
+};
+
+// reducer
+const reducer = reducerWithInitialState(INITIAL_STATE)
+  .case(subscribeProgram, (state, payload) => ({
+    ...state,
+    programs: [...payload],
+  }))
+  .case(update.async.started, (state) => ({
+    ...state,
+    isLoading: true,
+  }))
+  .case(update.async.done, (state) => ({
+    ...state,
+    isLoading: false,
+  }))
+  .case(update.async.failed, (state) => ({
+    ...state,
+    isLoading: false,
+  }));
+export default reducer;
+
+// selector
+export const selectPrograms = createSelector(
+  [(state: RootState) => state.domain.programs.programs],
+  (programs) => programs,
+);
+export const selectIsLoading = createSelector(
+  [(state: RootState) => state.domain.programs.isLoading],
+  (isLoading) => isLoading,
+);
