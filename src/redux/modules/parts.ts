d784fad82398088a65f3e4534bdb90b5b3f02923
hakushun <hakushun.pianist@gmail.com>
2021-06-14 17:41:09 +0900

    feat: parts.ts実装

---
+++ src/redux/modules/parts.ts
@@ -0,0 +1,68 @@
+import { createSelector } from 'reselect';
+import actionCreatorFactory from 'typescript-fsa';
+import { reducerWithInitialState } from 'typescript-fsa-reducers';
+import { asyncFactory } from 'typescript-fsa-redux-thunk';
+import { RootState } from './reducers';
+import { Part } from './part';
+import { updatePart } from '../../libs/firestore/crudPart';
+import { Event } from './event';
+
+export type Parts = {
+  parts: Part[];
+  isLoading: boolean;
+};
+export type UpdatePayload = {
+  event: Event;
+  part: Part[];
+};
+interface CustomError extends Error {
+  name: string;
+  message: string;
+}
+// action
+const actionCreator = actionCreatorFactory();
+const asyncActionCreator = asyncFactory<Parts>(actionCreator);
+
+export const subscribePart = actionCreator<Part[]>('SUBSCRIBE_PART');
+export const update = asyncActionCreator<UpdatePayload, void, CustomError>(
+  'UPDATE_PART',
+  async (payload) => {
+    await updatePart(payload);
+  },
+);
+
+// initial state
+const INITIAL_STATE: Parts = {
+  parts: [],
+  isLoading: false,
+};
+
+// reducer
+const reducer = reducerWithInitialState(INITIAL_STATE)
+  .case(subscribePart, (state, payload) => ({
+    ...state,
+    parts: [...payload],
+  }))
+  .case(update.async.started, (state) => ({
+    ...state,
+    isLoading: true,
+  }))
+  .case(update.async.done, (state) => ({
+    ...state,
+    isLoading: false,
+  }))
+  .case(update.async.failed, (state) => ({
+    ...state,
+    isLoading: false,
+  }));
+export default reducer;
+
+// selector
+export const selectParts = createSelector(
+  [(state: RootState) => state.domain.parts.parts],
+  (parts) => parts,
+);
+export const selectIsLoading = createSelector(
+  [(state: RootState) => state.domain.parts.isLoading],
+  (isLoading) => isLoading,
+);
