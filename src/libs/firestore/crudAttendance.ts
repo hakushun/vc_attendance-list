7c0c908f553f4e3e3c9bf44d00302f75182fc73b
hakushun <hakushun.pianist@gmail.com>
2021-06-16 10:59:13 +0900

    feat: crudAttendance.ts実装

---
+++ src/libs/firestore/crudAttendance.ts
@@ -0,0 +1,31 @@
+import { CrudPayload } from '../../redux/modules/attendances';
+import { getInstance } from './getInstance';
+
+const db = getInstance();
+
+export const createAttendance = async ({ eventId, attendance }: CrudPayload): Promise<void> => {
+  await db
+    .collection('attendances')
+    .doc(eventId)
+    .collection('attendance')
+    .doc(attendance.userId)
+    .set(attendance);
+};
+
+export const updateAttendance = async ({ eventId, attendance }: CrudPayload): Promise<void> => {
+  await db
+    .collection('attendances')
+    .doc(eventId)
+    .collection('attendance')
+    .doc(attendance.userId)
+    .set(attendance, { merge: true });
+};
+
+export const removeAttendance = async ({ eventId, attendance }: CrudPayload): Promise<void> => {
+  await db
+    .collection('attendances')
+    .doc(eventId)
+    .collection('attendance')
+    .doc(attendance.userId)
+    .delete();
+};
