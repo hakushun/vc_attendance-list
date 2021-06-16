4bb2c52351943826a1d150cb99f934a98c6385f9
hakushun <hakushun.pianist@gmail.com>
2021-06-16 11:00:15 +0900

    feat: attendances.ts実装

---
+++ src/redux/modules/attendances.ts
@@ -0,0 +1,83 @@
+import { createSelector } from 'reselect';
+import actionCreatorFactory from 'typescript-fsa';
+import { reducerWithInitialState } from 'typescript-fsa-reducers';
+import { asyncFactory } from 'typescript-fsa-redux-thunk';
+import { RootState } from './reducers';
+import { Attendance } from './attendance';
+import {
+  createAttendance,
+  removeAttendance,
+  updateAttendance,
+} from '../../libs/firestore/crudAttendance';
+
+export type Attendances = {
+  attendances: Attendance[];
+  isLoading: boolean;
+};
+export type CrudPayload = {
+  eventId: string;
+  attendance: Attendance;
+};
+interface CustomError extends Error {
+  name: string;
+  message: string;
+}
+// action
+const actionCreator = actionCreatorFactory();
+const asyncActionCreator = asyncFactory<Attendances>(actionCreator);
+
+export const subscribeAtendances = actionCreator<Attendance[]>('SUBSCRIBE_ATTENDANCES');
+export const create = asyncActionCreator<CrudPayload, void, CustomError>(
+  'CREATE_ATTENDANCE',
+  async (payload) => {
+    createAttendance(payload);
+  },
+);
+export const update = asyncActionCreator<CrudPayload, void, CustomError>(
+  'UPDATE_ATTENDANCE',
+  async (payload) => {
+    updateAttendance(payload);
+  },
+);
+export const remove = asyncActionCreator<CrudPayload, void, CustomError>(
+  'REMOVE_ATTENDANCE',
+  async (payload) => {
+    removeAttendance(payload);
+  },
+);
+// initial state
+const INITIAL_STATE: Attendances = {
+  attendances: [],
+  isLoading: false,
+};
+
+// reducer
+const reducer = reducerWithInitialState(INITIAL_STATE)
+  .case(subscribeAtendances, (state, payload) => ({
+    ...state,
+    attendances: payload,
+  }))
+  .cases([create.async.started, update.async.started, remove.async.started], (state) => ({
+    ...state,
+    isLoading: true,
+  }))
+  .cases([create.async.done, update.async.done, remove.async.done], (state) => ({
+    ...state,
+    isLoading: false,
+  }))
+  .cases([create.async.failed, update.async.failed, remove.async.failed], (state) => ({
+    ...state,
+    isLoading: false,
+  }));
+export default reducer;
+
+// selector
+export const selectAttendances = createSelector(
+  [(state: RootState) => state.domain.attendances.attendances],
+  // TODO: part順にsort
+  (attendances) => attendances,
+);
+export const selectIsLoading = createSelector(
+  [(state: RootState) => state.domain.attendances.isLoading],
+  (isLoading) => isLoading,
+);
