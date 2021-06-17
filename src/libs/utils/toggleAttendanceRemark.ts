de4bfb0e373ffa1e25d822ad0422060bdfb9b25d
hakushun <hakushun.pianist@gmail.com>
2021-06-17 17:10:29 +0900

    feat: toggleAttendanceRemark.ts実装

---
+++ src/libs/utils/toggleAttendanceRemark.ts
@@ -0,0 +1,13 @@
+export const toggleAttendanceRemark = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>): void => {
+  e.stopPropagation();
+  const parent = e.currentTarget.parentElement;
+  const target = parent?.querySelector('[data-type="remark"]');
+
+  if(!target) return;
+
+  if(target.getAttribute('data-is-shown') === 'false') {
+    target.setAttribute('data-is-shown', 'true');
+    return;
+  }
+  target.setAttribute('data-is-shown', 'false');
+}
