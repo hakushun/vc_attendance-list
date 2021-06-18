f2f51e2ce8eac161f95a35ff231edabd7cb61299
hakushun <hakushun.pianist@gmail.com>
2021-06-18 16:47:04 +0900

    refactor: createRoleの引数修正

--- src/libs/firestore/crudRole.ts
+++ src/libs/firestore/crudRole.ts
@@ -1,11 +1,12 @@
+import { Event } from '../../redux/modules/event';
 import { UpdatePayload } from '../../redux/modules/roles';
 import { getInstance } from './getInstance';
 
 const db = getInstance();
 
 // TODO: try/catch
-export const createRole = async (eventId: string): Promise<void> => {
-  await db.collection('roles').doc(eventId).set({ roles: [] });
+export const createRole = async (event: Event): Promise<void> => {
+  await db.collection('roles').doc(event.id).set({ roles: [] });
 };
 
 export const updateRole = async ({ eventId, roles }: UpdatePayload): Promise<void> => {
