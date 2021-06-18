8649381bcd390f7e516b9e42b5df9d2101c6294d
hakushun <hakushun.pianist@gmail.com>
2021-06-18 10:32:45 +0900

    feat: StringWithUrlコンポーネント作成

---
+++ src/components/uiParts/StringWithUrl/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+type Props = {
+  content: string;
+};
+export const StringWithUrl: React.VFC<Props> = ({ content }) => {
+  const regexp = /(https?:\/\/[\x21-\x7e]+)/g;
+
+  return (
+    <>
+      {content.split('\n').map((cntnt) =>
+        cntnt.match(regexp) ? (
+          <>
+            <a href={cntnt} target="_blank" rel="noopener noreferrer">
+              {cntnt}
+            </a>
+            <br />
+          </>
+        ) : (
+          <>
+            {cntnt}
+            <br />
+          </>
+        ),
+      )}
+    </>
+  );
+};
