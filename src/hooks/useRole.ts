02d507d962b7fadf41f91f5862a6308ac1b2b297
hakushun <hakushun.pianist@gmail.com>
2021-06-18 16:44:43 +0900

    feat: useRole.ts実装

---
+++ src/hooks/useRole.ts
@@ -0,0 +1,25 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { changeRadio, changeRole, RoleItem, selectProgramId, selectRoles } from '../redux/modules/role';
+
+type Hooks = {
+  programId: string;
+  roles: RoleItem[];
+  handleChangeRadio: (_e: React.ChangeEvent<HTMLInputElement>) => void;
+  handleChangeRole: (_e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+export const useRole = (): Hooks => {
+  const dispatch = useDispatch();
+  const programId = useSelector(selectProgramId);
+  const roles = useSelector(selectRoles);
+
+  const handleChangeRadio = (e: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(changeRadio({ programId: e.target.value }));
+  };
+
+  const handleChangeRole = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const userId = e.target.id.split('-')[1];
+    dispatch(changeRole({ userId, [programId]: e.target.value }));
+  };
+
+  return { programId, roles, handleChangeRadio, handleChangeRole };
+};
