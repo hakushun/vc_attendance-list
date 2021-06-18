0f7f96e71cd0f2b3dc59241eab16c051cedbce40
hakushun <hakushun.pianist@gmail.com>
2021-06-18 09:30:45 +0900

    feat: usePractice.ts にhandleFocusPractice実装

--- src/hooks/usePractice.ts
+++ src/hooks/usePractice.ts
@@ -5,7 +5,9 @@ import { selectEvent } from '../redux/modules/event';
 import { selectLocations } from '../redux/modules/locations';
 import { selectPlans } from '../redux/modules/plans';
 import {
+  focusPractice,
   PracticeItem,
+  selectDateId,
   selectIsLoading,
   selectPractice,
   subscribePractice,
@@ -15,7 +17,9 @@ import { selectRemarks } from '../redux/modules/remarks';
 
 type Hooks = {
   practice: PracticeItem;
+  dateId: string;
   isLoading: boolean;
+  handleFocusPractice: (_id: string) => void;
   handleUpdate: (_e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
 };
 export const usePractice = (eventId: string): Hooks => {
@@ -26,7 +30,12 @@ export const usePractice = (eventId: string): Hooks => {
   const plans = useSelector(selectPlans);
   const remarks = useSelector(selectRemarks);
   const practice = useSelector(selectPractice);
+  const dateId = useSelector(selectDateId);
   const isLoading = useSelector(selectIsLoading);
+
+  const handleFocusPractice = (id: string) => {
+    dispatch(focusPractice(id));
+  };
 
   const handleUpdate = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     e.preventDefault();
@@ -45,5 +54,5 @@ export const usePractice = (eventId: string): Hooks => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [eventId]);
 
-  return { practice, isLoading, handleUpdate };
+  return { practice, dateId, isLoading, handleFocusPractice, handleUpdate };
 };
