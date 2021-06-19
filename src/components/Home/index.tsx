805eec47b9356bdaa8a105997379dc5912023499
hakushun <hakushun.pianist@gmail.com>
2021-06-19 20:23:58 +0900

    feat: Homeコンポーネント修正

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import { useUser } from '../../hooks/useUser';
 import { Loading } from '../uiParts/Loading';
+import { EventForm } from '../EventForm';
+import { EventList } from './EventList';
 
 export const Home: React.VFC = () => {
   const { isLoading } = useUser();
 
   if (isLoading) return <Loading />;
 
-  return <section>HOME</section>;
+  return (
+    <>
+      <EventForm />
+      <EventList />
+    </>
+  );
 };
