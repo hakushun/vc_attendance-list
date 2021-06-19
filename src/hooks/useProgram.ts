14bf67a4028e7a313ec2dcfb25bb719fb0987cc0
hakushun <hakushun.pianist@gmail.com>
2021-06-19 15:07:20 +0900

    feat: useProgram.tsにhandleFocusProgram実装

--- src/hooks/useProgram.ts
+++ src/hooks/useProgram.ts
@@ -3,19 +3,24 @@ import {
   addProgramForm,
   changeProgram,
   deleteProgramForm,
+  focusProgram,
   ProgramItem,
   selectProgram,
+  selectSelectedId,
 } from '../redux/modules/program';
 
 type Hooks = {
   program: ProgramItem[];
+  selectedId: string;
   handleChange: (_e: React.ChangeEvent<HTMLInputElement>) => void;
   handleAddProgramForm: () => void;
   handleDeleteProgramForm: () => void;
+  handleFocusProgram: (_e: React.ChangeEvent<HTMLSelectElement>) => void;
 };
 export const useProgram = (): Hooks => {
   const dispatch = useDispatch();
   const program = useSelector(selectProgram);
+  const selectedId = useSelector(selectSelectedId);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const id = e.target.id.split('-')[1];
@@ -30,5 +35,16 @@ export const useProgram = (): Hooks => {
     dispatch(deleteProgramForm());
   };
 
-  return { program, handleChange, handleAddProgramForm, handleDeleteProgramForm };
+  const handleFocusProgram = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    dispatch(focusProgram(e.target.value));
+  };
+
+  return {
+    program,
+    selectedId,
+    handleChange,
+    handleAddProgramForm,
+    handleDeleteProgramForm,
+    handleFocusProgram,
+  };
 };
