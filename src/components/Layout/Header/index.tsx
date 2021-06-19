02ceebb25d9660997369069691e33f52835ab2f9
hakushun <hakushun.pianist@gmail.com>
2021-06-19 20:27:09 +0900

    feat: navをパスによって出し分け

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import React from 'react';
+import { useRouter } from '../../../hooks/useRouter';
 import { useShow } from '../../../hooks/useShow';
 import { useSign } from '../../../hooks/useSign';
 import { useUser } from '../../../hooks/useUser';
@@ -7,9 +8,10 @@ import { QuaternaryButton } from '../../uiParts/QuaternaryButton';
 import styles from './index.module.scss';
 
 export const Header: React.VFC = () => {
+  const { router } = useRouter();
   const { user } = useUser();
   const { handleSignOut } = useSign();
-  const { handleToggleEventFormWithInitiate, handleToggleSetting } = useShow();
+  const { handleToggleEventForm, handleToggleSetting } = useShow();
 
   return (
     <header className={styles.root}>
@@ -22,12 +24,16 @@ export const Header: React.VFC = () => {
         {user && (
           <nav className={styles.nav}>
             <ul className={styles.list}>
-              <li>
-                <QuaternaryButton
-                  label="イベント作成"
-                  handleClick={handleToggleEventFormWithInitiate}
-                />
-              </li>
+              {router.pathname === '/' && (
+                <li>
+                  <QuaternaryButton label="イベント作成" handleClick={handleToggleEventForm} />
+                </li>
+              )}
+              {router.pathname === '/event/[id]' && (
+                <li>
+                  <QuaternaryButton label="イベント編集" handleClick={handleToggleEventForm} />
+                </li>
+              )}
               {/* TODO: useIdによる出し分け */}
               <li>
                 <QuaternaryButton label="設定" handleClick={handleToggleSetting} />
