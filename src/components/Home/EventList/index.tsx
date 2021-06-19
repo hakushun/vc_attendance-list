825a4091a08014259f84f086752e8f0944b52b22
hakushun <hakushun.pianist@gmail.com>
2021-06-19 20:24:27 +0900

    feat: buttonからLinkに変k脳

--- src/components/Home/EventList/index.tsx
+++ src/components/Home/EventList/index.tsx
@@ -1,12 +1,11 @@
+import Link from 'next/link';
 import React from 'react';
-import { useEvent } from '../../../hooks/useEvent';
 import { useEvents } from '../../../hooks/useEvents';
 import { Heading } from '../../uiParts/Heading';
 import { Sectioning } from '../../uiParts/Sectioning';
 import styles from './index.module.scss';
 
 export const EventList: React.VFC = () => {
-  const { handleFocusEvent } = useEvent();
   const { events } = useEvents();
 
   return (
@@ -15,9 +14,9 @@ export const EventList: React.VFC = () => {
       <ul className={styles.list}>
         {events.map((event) => (
           <li key={event.id}>
-            <button type="button" className={styles.button} onClick={() => handleFocusEvent(event)}>
-              {event.title}
-            </button>
+            <Link href={`/event/${event.id}`}>
+              <a className={styles.link}>{event.title}</a>
+            </Link>
           </li>
         ))}
       </ul>
