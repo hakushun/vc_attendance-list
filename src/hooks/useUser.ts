25eabedc12c7989725184e009b78dd0baa5ee28e
hakushun <hakushun.pianist@gmail.com>
2021-06-19 20:22:55 +0900

    refactor: useRouterの差し替え

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -1,10 +1,10 @@
 import firebase from 'firebase/app';
-import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import initFirebase from '../libs/firebase/initFirebase';
 import { mapUserData } from '../libs/utils/mapUserData';
 import { selectIsLoading, selectUser, auth, Userdata } from '../redux/modules/user';
+import { useRouter } from './useRouter';
 
 initFirebase();
 
@@ -14,7 +14,7 @@ type Hooks = {
 };
 export const useUser = (): Hooks => {
   const dispatch = useDispatch();
-  const router = useRouter();
+  const { router } = useRouter();
   const user = useSelector(selectUser);
   const isLoading = useSelector(selectIsLoading);
 
