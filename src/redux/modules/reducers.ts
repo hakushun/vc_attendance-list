4a3ba3bef61fd139ee5f61d73344744729577359
hakushun <hakushun.pianist@gmail.com>
2021-06-19 20:28:24 +0900

    feat: commit忘れ

--- src/redux/modules/reducers.ts
+++ src/redux/modules/reducers.ts
@@ -1,8 +1,46 @@
 import { combineReducers } from 'redux';
+import attendance from './attendance';
+import attendances from './attendances';
+import event from './event';
+import events from './events';
+import locations from './locations';
+import part from './part';
+import parts from './parts';
+import plans from './plans';
+import practice from './practice';
+import program from './program';
+import programs from './programs';
+import remarks from './remarks';
+import role from './role';
+import roles from './roles';
+import user from './user';
+import modal from './modal';
+import show from './show';
+import sign from './sign';
+import tab from './tab';
 
 const rootReducer = combineReducers({
-  resources: combineReducers({}),
-  ui: combineReducers({}),
+  domain: combineReducers({
+    attendances,
+    events,
+    parts,
+    practice,
+    programs,
+    roles,
+  }),
+  app: combineReducers({
+    attendance,
+    event,
+    locations,
+    part,
+    plans,
+    program,
+    remarks,
+    role,
+    sign,
+    user,
+  }),
+  ui: combineReducers({ modal, show, tab }),
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
