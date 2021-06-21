0406295da959e7e1c5cb001e2ece8d7706635e36
hakushun <hakushun.pianist@gmail.com>
2021-06-21 13:49:10 +0900

    feat: isAttendanceInvalid.ts実装

---
+++ src/libs/utils/isAttendanceInvalid.ts
@@ -0,0 +1,5 @@
+import { Attendance } from '../../redux/modules/attendance';
+
+export const isAttendanceInvalid = (attendance: Attendance): boolean => {
+  return attendance.name.trim() === '' || attendance.part === '' || !attendance.userId;
+};
