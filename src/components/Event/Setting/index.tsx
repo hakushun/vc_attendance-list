055a975dff6e5641dfa3aa65b5a337e633bfa4e1
hakushun <hakushun.pianist@gmail.com>
2021-06-21 10:41:19 +0900

    refactor: 子ページにpropsを渡せるようにcustom hooksと接続

--- src/components/Event/Setting/index.tsx
+++ src/components/Event/Setting/index.tsx
@@ -7,21 +7,55 @@ import { Program } from './Program';
 import { Part } from './Part';
 import { Role } from './Role';
 import { Event } from '../../../redux/modules/event';
-import { Tab } from '../../../redux/modules/tab';
 import styles from './index.module.scss';
+import { usePart } from '../../../hooks/usePart';
+import { useParts } from '../../../hooks/useParts';
+import { useAttendances } from '../../../hooks/useAttendances';
+import { usePrograms } from '../../../hooks/usePrograms';
+import { useRole } from '../../../hooks/useRole';
+import { useRoles } from '../../../hooks/useRoles';
+import { useProgram } from '../../../hooks/useProgram';
+import { useLocations } from '../../../hooks/useLocations';
+import { usePlans } from '../../../hooks/usePlans';
+import { usePractice } from '../../../hooks/usePractice';
+import { useRemarks } from '../../../hooks/useRemarks';
+import { useTab } from '../../../hooks/useTab';
 
 type Props = {
   event: Event;
   handleToggleSetting: () => void;
-  tab: Tab;
-  handleChangeSettingTab: (_e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
 };
-export const Setting: React.VFC<Props> = ({
-  event,
-  handleToggleSetting,
-  tab,
-  handleChangeSettingTab,
-}) => {
+export const Setting: React.VFC<Props> = ({ event, handleToggleSetting }) => {
+  const {
+    part,
+    handleChange: handlePartChange,
+    handleAddPartForm,
+    handleDeletePartForm,
+    handleChangeOrder,
+  } = usePart();
+  const { attendances } = useAttendances(event.id);
+  const { parts, isLoading: PartsIsLoading, handleUpdate: handlePartsUpdate } = useParts(event.id);
+  const {
+    programs,
+    isLoading: ProgramsIsLoading,
+    handleUpdate: handleProgramsUpdate,
+  } = usePrograms(event.id);
+  const { programId, roles, handleChangeRadio, handleChangeRole } = useRole();
+  const { isLoading: RolesIsLoading, handleUpdate: handleRolesUpdate } = useRoles(event.id);
+  const {
+    program,
+    handleChange: handleProgramChange,
+    handleAddProgramForm,
+    handleDeleteProgramForm,
+  } = useProgram();
+  const { locations, handleChangeLocations } = useLocations();
+  const { plans, handleChangePlans } = usePlans();
+  const { remarks, handleChangeRemarks } = useRemarks();
+  const { isLoading: PracticeIsLoading, handleUpdate: handlePracticeUpdate } = usePractice(
+    event.id!,
+  );
+  const { tab, handleChangeSettingTab } = useTab();
+
   return (
     <Sectioning id="setting">
       <Heading level={2} label="各種設定" />
@@ -66,16 +100,56 @@ export const Setting: React.VFC<Props> = ({
         </div>
         <div role="tabpanel" id={tab.setting} className={styles.tabpanel}>
           {tab.setting === 'practice' && (
-            <Practice event={event} handleToggleSetting={handleToggleSetting} />
+            <Practice
+              event={event}
+              handleToggleSetting={handleToggleSetting}
+              locations={locations}
+              handleChangeLocations={handleChangeLocations}
+              plans={plans}
+              handleChangePlans={handleChangePlans}
+              remarks={remarks}
+              handleChangeRemarks={handleChangeRemarks}
+              isLoading={PracticeIsLoading}
+              handleUpdate={handlePracticeUpdate}
+            />
           )}
           {tab.setting === 'program' && (
-            <Program event={event} handleToggleSetting={handleToggleSetting} />
+            <Program
+              event={event}
+              handleToggleSetting={handleToggleSetting}
+              program={program}
+              handleChange={handleProgramChange}
+              handleAddProgramForm={handleAddProgramForm}
+              handleDeleteProgramForm={handleDeleteProgramForm}
+              isLoading={ProgramsIsLoading}
+              handleUpdate={handleProgramsUpdate}
+            />
           )}
           {tab.setting === 'role' && (
-            <Role event={event} handleToggleSetting={handleToggleSetting} />
+            <Role
+              handleToggleSetting={handleToggleSetting}
+              programs={programs}
+              parts={parts}
+              attendances={attendances}
+              programId={programId}
+              roles={roles}
+              handleChangeRadio={handleChangeRadio}
+              handleChangeRole={handleChangeRole}
+              isLoading={RolesIsLoading}
+              handleUpdate={handleRolesUpdate}
+            />
           )}
           {tab.setting === 'part' && (
-            <Part event={event} handleToggleSetting={handleToggleSetting} />
+            <Part
+              handleToggleSetting={handleToggleSetting}
+              part={part}
+              handleChange={handlePartChange}
+              handleAddPartForm={handleAddPartForm}
+              handleDeletePartForm={handleDeletePartForm}
+              handleChangeOrder={handleChangeOrder}
+              isLoading={PartsIsLoading}
+              handleUpdate={handlePartsUpdate}
+            />
           )}
         </div>
       </div>
