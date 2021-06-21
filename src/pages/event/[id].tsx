6f81f6a28bf8ab62ae5c302a252dff008f9645f7
hakushun <hakushun.pianist@gmail.com>
2021-06-21 10:43:51 +0900

    feat: event/[id]ページにSSR導入

--- src/pages/event/[id].tsx
+++ src/pages/event/[id].tsx
@@ -1,12 +1,15 @@
-import { NextPage } from 'next';
+import { GetServerSideProps, NextPage } from 'next';
 import React from 'react';
 import { Event } from '../../components/Event';
-import { useRouter } from '../../hooks/useRouter';
 
-const EventPage: NextPage = () => {
-  const { router } = useRouter();
-  const { id } = router.query;
-
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { id } = context.query;
+  return { props: { id } };
+};
+type Props = {
+  id: string;
+};
+const EventPage: NextPage<Props> = ({ id }) => {
   return <Event eventId={typeof id === 'string' ? id : ''} />;
 };
 
