7134818e33461f6d5bfb7f19aef50c66aa4c20c8
hakushun <hakushun.pianist@gmail.com>
2021-06-21 10:43:32 +0900

    feat: ページ遷移時にcloseAllとcloseAllModalが発火するように修正

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { AppProps } from 'next/app';
+import { useDispatch } from 'react-redux';
 import { wrapper } from '../redux/store';
+import { closeAll } from '../redux/modules/show';
+import { useRouter } from '../hooks/useRouter';
 import { Layout } from '../components/Layout';
 
 import '../styles/reset.scss';
 import '../styles/global.scss';
+import { closeAllModal } from '../redux/modules/modal';
 
 function App({ Component, pageProps }: AppProps): React.ReactElement {
+  const { router } = useRouter();
+  const dispatch = useDispatch();
+
+  const handleRouteChange = () => {
+    const main = document.getElementById('main');
+    main?.focus();
+    dispatch(closeAll());
+    dispatch(closeAllModal());
+  };
+
+  useEffect(() => {
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => router.events.off('routeChangeComplete', handleRouteChange);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <>
       <Layout>
