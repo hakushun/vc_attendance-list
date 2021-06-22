f65bc34f72ea07bafabf9d6d6451b29a60b3bfc9
hakushun <hakushun.pianist@gmail.com>
2021-06-22 19:39:07 +0900

    feat: modal.tsにcovidResult追加

--- src/redux/modules/modal.ts
+++ src/redux/modules/modal.ts
@@ -1,21 +1,24 @@
 import { createSelector } from 'reselect';
 import actionCreatorFactory from 'typescript-fsa';
 import { reducerWithInitialState } from 'typescript-fsa-reducers';
+import { fetch } from './covids';
 import { focusPractice } from './practice';
 import { RootState } from './reducers';
 
-type KeyName = 'practice';
+type KeyName = 'practice' | 'covidResult';
 export type Modal = Record<KeyName, boolean>;
 
 // action
 const actionCreator = actionCreatorFactory();
 
 export const togglePracticeModal = actionCreator<boolean>('TOGGLE_PRACTICE_MODAL');
+export const toggleCovidResult = actionCreator<boolean>('TOGGLE_COVID_RESULT');
 export const closeAllModal = actionCreator('CLOSE_ALL_MODAL');
 
 // initial state
 const INITIAL_STATE: Modal = {
   practice: false,
+  covidResult: false,
 };
 
 // reducer
@@ -24,10 +27,18 @@ const reducer = reducerWithInitialState(INITIAL_STATE)
     ...state,
     practice: payload,
   }))
+  .case(toggleCovidResult, (state, payload) => ({
+    ...state,
+    covidResult: payload,
+  }))
   .case(closeAllModal, () => ({ ...INITIAL_STATE }))
   .case(focusPractice, (state) => ({
     ...state,
     practice: true,
+  }))
+  .case(fetch.async.started, (state) => ({
+    ...state,
+    covidResult: true,
   }));
 export default reducer;
 
@@ -35,4 +46,8 @@ export default reducer;
 export const selectPracticeModalIsShown = createSelector(
   [(state: RootState) => state.ui.modal.practice],
   (practice) => practice,
+);
+export const selectCovidResultIsShown = createSelector(
+  [(state: RootState) => state.ui.modal.covidResult],
+  (covidResult) => covidResult,
 );
