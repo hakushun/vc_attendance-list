5901bf8b0e4a1d5fca2161b256246ac4a5aac0c7
hakushun <hakushun.pianist@gmail.com>
2021-06-22 19:41:06 +0900

    feat: useCovids.tsに接続

--- src/hooks/useCovids.ts
+++ src/hooks/useCovids.ts
@@ -1,10 +1,20 @@
 import { useDispatch, useSelector } from 'react-redux';
+import { Attendance } from '../redux/modules/attendance';
 import { Covid, selectCovid } from '../redux/modules/covid';
-import { create, fetch, selectCovids, selectIsLoading } from '../redux/modules/covids';
+import {
+  create,
+  fetch,
+  selectAnswerResult,
+  selectCovids,
+  selectIsLoading,
+  selectUnansweredUsers,
+} from '../redux/modules/covids';
 
 type Hooks = {
   covids: Covid[];
   isLoading: boolean;
+  answerRuselt: (Covid & { name: string | undefined; part: string | undefined })[];
+  unasweredUsers: Attendance[];
   handleFetch: (_dateId: string) => void;
   handleCreate: (_e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
 };
@@ -13,6 +23,8 @@ export const useCovids = (eventId: string): Hooks => {
   const covid = useSelector(selectCovid);
   const covids = useSelector(selectCovids);
   const isLoading = useSelector(selectIsLoading);
+  const answerRuselt = useSelector(selectAnswerResult);
+  const unasweredUsers = useSelector(selectUnansweredUsers);
 
   const handleFetch = (dateId: string) => {
     dispatch(fetch({ eventId, dateId }));
@@ -22,5 +34,5 @@ export const useCovids = (eventId: string): Hooks => {
     e.preventDefault();
     dispatch(create({ eventId, covid }));
   };
-  return { covids, isLoading, handleFetch, handleCreate };
+  return { covids, isLoading, answerRuselt, unasweredUsers, handleFetch, handleCreate };
 };
