5234f0192a4c8393a48bf190ceeeb9e20c2b9117
hakushun <hakushun.pianist@gmail.com>
2021-06-23 13:32:46 +0900

    feat: modalにpasswordReset追加

--- src/redux/modules/modal.ts
+++ src/redux/modules/modal.ts
@@ -4,8 +4,9 @@ import { reducerWithInitialState } from 'typescript-fsa-reducers';
 import { fetch } from './covids';
 import { focusPractice } from './practice';
 import { RootState } from './reducers';
+import { resetPassword } from './sign';
 
-type KeyName = 'practice' | 'covidResult';
+type KeyName = 'practice' | 'covidResult' | 'passwordReset';
 export type Modal = Record<KeyName, boolean>;
 
 // action
@@ -13,12 +14,14 @@ const actionCreator = actionCreatorFactory();
 
 export const togglePracticeModal = actionCreator<boolean>('TOGGLE_PRACTICE_MODAL');
 export const toggleCovidResult = actionCreator<boolean>('TOGGLE_COVID_RESULT');
+export const togglePasswordReset = actionCreator<boolean>('TOGGLE_PASSWORD_RESET');
 export const closeAllModal = actionCreator('CLOSE_ALL_MODAL');
 
 // initial state
 const INITIAL_STATE: Modal = {
   practice: false,
   covidResult: false,
+  passwordReset: false,
 };
 
 // reducer
@@ -31,6 +34,10 @@ const reducer = reducerWithInitialState(INITIAL_STATE)
     ...state,
     covidResult: payload,
   }))
+  .case(togglePasswordReset, (state, payload) => ({
+    ...state,
+    passwordReset: payload,
+  }))
   .case(closeAllModal, () => ({ ...INITIAL_STATE }))
   .case(focusPractice, (state) => ({
     ...state,
@@ -39,6 +46,10 @@ const reducer = reducerWithInitialState(INITIAL_STATE)
   .case(fetch.async.started, (state) => ({
     ...state,
     covidResult: true,
+  }))
+  .case(resetPassword.async.done, (state) => ({
+    ...state,
+    passwordReset: false,
   }));
 export default reducer;
 
@@ -50,4 +61,8 @@ export const selectPracticeModalIsShown = createSelector(
 export const selectCovidResultIsShown = createSelector(
   [(state: RootState) => state.ui.modal.covidResult],
   (covidResult) => covidResult,
+);
+export const selectPasswordResetIsShown = createSelector(
+  [(state: RootState) => state.ui.modal.passwordReset],
+  (passwordReset) => passwordReset,
 );
