2ae39c76663de8cf675700f7a5d64271c5de509e
hakushun <hakushun.pianist@gmail.com>
2021-06-23 13:32:33 +0900

    feat: useSign.tsにresetForm関連接続

--- src/hooks/useSign.ts
+++ src/hooks/useSign.ts
@@ -1,5 +1,6 @@
 import { useDispatch, useSelector } from 'react-redux';
 import initFirebase from '../libs/firebase/initFirebase';
+import { isEmail } from '../libs/utils/isEmail';
 import { isSignFormInvalid } from '../libs/utils/isSignFormInvalid';
 import {
   signUp,
@@ -9,6 +10,9 @@ import {
   selectSignForm,
   SignForm,
   change,
+  selectResetForm,
+  changeResetForm,
+  resetPassword,
 } from '../redux/modules/sign';
 import { useRouter } from './useRouter';
 
@@ -16,22 +20,28 @@ initFirebase();
 
 type Hooks = {
   form: SignForm;
+  resetForm: { email: string };
   isLoading: boolean;
   handleChange: (_e: React.ChangeEvent<HTMLInputElement>) => void;
+  handleChangeResetForm: (_e: React.ChangeEvent<HTMLInputElement>) => void;
   handleSignUp: (_e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => Promise<void>;
   handleSignIn: (_e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => Promise<void>;
   handleSignOut: () => Promise<void>;
+  handleResetPassword: (_e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => Promise<void>;
 };
 export const useSign = (): Hooks => {
   const dispatch = useDispatch();
   const { router } = useRouter();
   const form = useSelector(selectSignForm);
+  const resetForm = useSelector(selectResetForm);
   const isLoading = useSelector(selectIsLoading);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     dispatch(change({ [e.target.id]: e.target.value }));
   };
-
+  const handleChangeResetForm = (e: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(changeResetForm({ email: e.target.value }));
+  };
   const handleSignUp = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     e.preventDefault();
     if (isSignFormInvalid(form)) return;
@@ -47,6 +57,20 @@ export const useSign = (): Hooks => {
   const handleSignOut = async () => {
     await dispatch(signOut());
   };
-
-  return { form, isLoading, handleChange, handleSignUp, handleSignIn, handleSignOut };
+  const handleResetPassword = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    e.preventDefault();
+    if(!isEmail(resetForm.email)) return;
+    await dispatch(resetPassword(resetForm));
+  };
+  return {
+    form,
+    resetForm,
+    isLoading,
+    handleChange,
+    handleChangeResetForm,
+    handleSignUp,
+    handleSignIn,
+    handleSignOut,
+    handleResetPassword,
+  };
 };
