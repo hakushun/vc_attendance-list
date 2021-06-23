945576cd81fe02670dd22ac46d0855bcffab2946
hakushun <hakushun.pianist@gmail.com>
2021-06-23 14:49:38 +0900

    refactor: EventListコンポーネント分割

---
+++ src/components/Home/EventList/EventList.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link';
+import React from 'react';
+import { Event } from '../../../redux/modules/event';
+import { Heading } from '../../uiParts/Heading';
+import { Sectioning } from '../../uiParts/Sectioning';
+import styles from './index.module.scss';
+
+type Props = {
+  events: Event[];
+};
+export const EventList: React.VFC<Props> = ({ events }) => {
+  return (
+    <Sectioning id="event_list">
+      <Heading level={2} label="イベント一覧" />
+      <ul className={styles.list}>
+        {events.map((event) => (
+          <li key={event.id}>
+            <Link href={`/event/${event.id}`}>
+              <a className={styles.link}>{event.title}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </Sectioning>
+  );
+};
--- src/components/Home/EventList/index.tsx
+++ src/components/Home/EventList/index.tsx
@@ -1,25 +1,9 @@
-import Link from 'next/link';
 import React from 'react';
 import { useEvents } from '../../../hooks/useEvents';
-import { Heading } from '../../uiParts/Heading';
-import { Sectioning } from '../../uiParts/Sectioning';
-import styles from './index.module.scss';
+import { EventList as Presentational } from './EventList';
 
 export const EventList: React.VFC = () => {
   const { events } = useEvents();
 
-  return (
-    <Sectioning id="event_list">
-      <Heading level={2} label="イベント一覧" />
-      <ul className={styles.list}>
-        {events.map((event) => (
-          <li key={event.id}>
-            <Link href={`/event/${event.id}`}>
-              <a className={styles.link}>{event.title}</a>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </Sectioning>
-  );
+  return <Presentational events={events} />;
 };
