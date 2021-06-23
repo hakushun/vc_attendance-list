076a04a37c8820d7c3c5f2e5e61fb60cbcbb171e
hakushun <hakushun.pianist@gmail.com>
2021-06-23 15:34:21 +0900

    refactor: Hooksの呼び出し整理

--- src/components/Event/index.tsx
+++ src/components/Event/index.tsx
@@ -10,16 +10,13 @@ import { PracticeDetail } from './PracticeDetail';
 import { Setting } from './Setting';
 import { useEvents } from '../../hooks/useEvents';
 import { useEvent } from '../../hooks/useEvent';
-import { useAttendance } from '../../hooks/useAttendance';
 import { useAttendances } from '../../hooks/useAttendances';
 import { useParts } from '../../hooks/useParts';
 import { usePractice } from '../../hooks/usePractice';
-import { useProgram } from '../../hooks/useProgram';
 import { usePrograms } from '../../hooks/usePrograms';
 import { useRoles } from '../../hooks/useRoles';
 import { useModal } from '../../hooks/useModal';
 import { CovidForm } from './CovidForm';
-import { useCovid } from '../../hooks/useCovid';
 import { useCovids } from '../../hooks/useCovids';
 import { CovidResult } from './CovidResult';
 
@@ -27,40 +24,36 @@ type Props = {
   eventId: string;
 };
 export const Event: React.VFC<Props> = ({ eventId }) => {
-  const { isLoading, user } = useUser();
+  // domain
+  const { parts, isLoading: partsIsLoading, handleUpdate: handlePartsUpdate } = useParts(eventId);
   const {
-    settingIsShown,
-    attendanceFormIsShown,
-    covidFormIsShown,
-    handleToggleSetting,
-    handleToggleAttendanceForm,
-    handleToggleCovidForm,
-  } = useShow();
-  const { targetEvent } = useEvents(eventId);
-  const { event } = useEvent(targetEvent);
-  const { parts } = useParts(eventId);
+    practice,
+    dateId,
+    isLoading: practiceIsLoading,
+    handleUpdate: handlePracticeUpdate,
+    handleFocusPractice,
+  } = usePractice(eventId);
   const {
-    attendance,
-    handleChangeAttendance,
-    handleChangeRemark,
-    handleClickRadio,
-    handleKeyDownRadio,
-    handleFocusAttendance,
-  } = useAttendance();
+    programs,
+    isLoading: programsIsLoading,
+    handleUpdate: handleProgramsUpdate,
+  } = usePrograms(eventId);
+  const { roles, isLoading: rolesIsLoading, handleUpdate: handleRolesUpdate } = useRoles(eventId);
+  const {
+    targetEvent,
+    isLoading: eventsIsLoading,
+    handleCreate: handleEventCreate,
+    handleUpdate: handleEventUpdate,
+    handleRemove: handleEventRemove,
+  } = useEvents(eventId);
   const {
     attendances,
     isLoading: AttendanceIsLoading,
     breakdownAttendances,
-    handleCreate,
-    handleUpdate,
-    handleRemove,
+    handleCreate: handleAttendanceCreate,
+    handleUpdate: handleAttendanceUpdate,
+    handleRemove: handleAttendanceRemove,
   } = useAttendances(eventId);
-  const { practice, dateId, handleFocusPractice } = usePractice(eventId);
-  const { selectedId, handleFocusProgram } = useProgram();
-  const { programs } = usePrograms(eventId);
-  const { roles } = useRoles(eventId);
-  const { practiceModalIsShown, covidResultIsShown } = useModal();
-  const { covid, handleChangeCovidDate, handleChangeCovidAnswers } = useCovid();
   const {
     isLoading: covidsIsLoading,
     answerRuselt,
@@ -69,48 +62,59 @@ export const Event: React.VFC<Props> = ({ eventId }) => {
     handleCreate: handleCovidCreate,
   } = useCovids(eventId);
 
+  // app
+  const { isLoading, user } = useUser();
+  const { event } = useEvent(targetEvent);
+
+  // ui
+  const { settingIsShown, handleToggleSetting } = useShow();
+  const { practiceModalIsShown, covidResultIsShown } = useModal();
+
   if (isLoading) return <Loading />;
 
   return (
     <>
-      {settingIsShown && <Setting event={event} handleToggleSetting={handleToggleSetting} />}
-      <EventForm />
+      {settingIsShown && (
+        <Setting
+          event={event}
+          handleToggleSetting={handleToggleSetting}
+          attendances={attendances}
+          parts={parts}
+          partsIsLoading={partsIsLoading}
+          handlePartsUpdate={handlePartsUpdate}
+          practiceIsLoading={practiceIsLoading}
+          handlePracticeUpdate={handlePracticeUpdate}
+          programs={programs}
+          programsIsLoading={programsIsLoading}
+          handleProgramsUpdate={handleProgramsUpdate}
+          rolesIsLoading={rolesIsLoading}
+          handleRolesUpdate={handleRolesUpdate}
+        />
+      )}
+      <EventForm
+        isLoading={eventsIsLoading}
+        handleCreate={handleEventCreate}
+        handleUpdate={handleEventUpdate}
+        handleRemove={handleEventRemove}
+      />
       <EventHeader event={event} />
       <AttendanceForm
         user={user}
-        attendanceFormIsShown={attendanceFormIsShown}
-        handleToggleAttendanceForm={handleToggleAttendanceForm}
         event={event}
         parts={parts}
-        attendance={attendance}
-        handleChangeAttendance={handleChangeAttendance}
-        handleChangeRemark={handleChangeRemark}
-        handleClickRadio={handleClickRadio}
-        handleKeyDownRadio={handleKeyDownRadio}
         attendances={attendances}
         AttendanceIsLoading={AttendanceIsLoading}
-        handleCreate={handleCreate}
-        handleUpdate={handleUpdate}
-        handleRemove={handleRemove}
+        handleCreate={handleAttendanceCreate}
+        handleUpdate={handleAttendanceUpdate}
+        handleRemove={handleAttendanceRemove}
       />
-      <CovidForm
-        event={event}
-        covidFormIsShown={covidFormIsShown}
-        handleToggleCovidForm={handleToggleCovidForm}
-        covid={covid}
-        handleChangeCovidDate={handleChangeCovidDate}
-        handleChangeCovidAnswers={handleChangeCovidAnswers}
-        handleCreate={handleCovidCreate}
-      />
+      <CovidForm event={event} handleCreate={handleCovidCreate} />
       <AttendanceTable
         user={user}
         event={event}
-        handleFocusAttendance={handleFocusAttendance}
         attendances={attendances}
         AttendanceIsLoading={AttendanceIsLoading}
         handleFocusPractice={handleFocusPractice}
-        selectedId={selectedId}
-        handleFocusProgram={handleFocusProgram}
         programs={programs}
         roles={roles}
         handleFetch={handleFetch}
