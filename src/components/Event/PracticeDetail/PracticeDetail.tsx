6df387f94e403c6a86b0a44fa81192f24abe0ad9
hakushun <hakushun.pianist@gmail.com>
2021-06-23 14:49:18 +0900

    refactor: PracticeDetailコンポーネント分割

---
+++ src/components/Event/PracticeDetail/PracticeDetail.tsx
@@ -0,0 +1,98 @@
+import React, { MutableRefObject } from 'react';
+import { Attendance } from '../../../redux/modules/attendance';
+import { PracticeItem } from '../../../redux/modules/practice';
+import { Heading } from '../../uiParts/Heading';
+import { Modal } from '../../uiParts/Modal';
+import { SecondaryButton } from '../../uiParts/SecondaryButton';
+import { StringWithUrl } from '../../uiParts/StringWithUrl';
+import styles from './index.module.scss';
+
+type Props = {
+  practice: PracticeItem;
+  dateId: string;
+  breakdownAttendances: {
+    presence: Attendance[];
+    undecided: Attendance[];
+    absence: Attendance[];
+  };
+  modalRef: MutableRefObject<HTMLElement | null>;
+  handleTogglePracticeModal: () => void;
+  handleKeydown: (_e: React.KeyboardEvent<HTMLElement>) => void;
+  titleDate: string;
+  titleDayOfTheWeek: string;
+  url: string | undefined;
+  content: string | undefined;
+};
+export const PracticeDetail: React.VFC<Props> = ({
+  practice,
+  dateId,
+  breakdownAttendances,
+  modalRef,
+  handleTogglePracticeModal,
+  handleKeydown,
+  titleDate,
+  titleDayOfTheWeek,
+  url,
+  content,
+}) => {
+  return (
+    <Modal modalRef={modalRef} handleKeydown={handleKeydown}>
+      <div className={styles.root}>
+        <Heading level={3} label={`${titleDate}${titleDayOfTheWeek}の詳細`} />
+        <dl className={styles.list}>
+          <div className={styles.item}>
+            <dt className={styles.term}>施設1</dt>
+            <dd className={styles.definition}>
+              {practice.locations.find((loc) => loc.dateId === dateId)?.name1 || '未入力'}
+            </dd>
+          </div>
+          <div className={styles.item}>
+            <dt className={styles.term}>施設2</dt>
+            <dd className={styles.definition}>
+              {practice.locations.find((loc) => loc.dateId === dateId)?.name2 || '未入力'}
+            </dd>
+          </div>
+          <div className={styles.item}>
+            <dt className={styles.term}>URL</dt>
+            <dd className={styles.definition}>
+              {url ? (
+                <a href={url} target="_blank" rel="noopener noreferrer">
+                  {url}
+                </a>
+              ) : (
+                '未入力'
+              )}
+            </dd>
+          </div>
+          <div className={styles.item}>
+            <dt className={styles.term}>区分</dt>
+            <dd className={styles.definition}>
+              {practice.plans.find((pln) => pln.dateId === dateId)?.category || '未入力'}
+            </dd>
+          </div>
+          <div className={styles.item}>
+            <dt className={styles.term}>スケジュール</dt>
+            <dd className={styles.definition}>
+              {practice.plans.find((pln) => pln.dateId === dateId)?.schedule || '未入力'}
+            </dd>
+          </div>
+          <div className={styles.item}>
+            <dt className={styles.term}>備考</dt>
+            <dd className={styles.definition}>
+              {content ? <StringWithUrl content={content} /> : '未入力'}
+            </dd>
+          </div>
+          <div className={styles.item}>
+            <dt className={styles.term}>出欠</dt>
+            <dd className={styles.definition}>
+              <div>○：{breakdownAttendances.presence.length}</div>
+              <div>△：{breakdownAttendances.undecided.length}</div>
+              <div>×：{breakdownAttendances.absence.length}</div>
+            </dd>
+          </div>
+        </dl>
+        <SecondaryButton label="閉じる" disabled={false} handleClick={handleTogglePracticeModal} />
+      </div>
+    </Modal>
+  );
+};
--- src/components/Event/PracticeDetail/index.tsx
+++ src/components/Event/PracticeDetail/index.tsx
@@ -5,11 +5,7 @@ import { getStringDate } from '../../../libs/dayjs/getStringDate';
 import { Attendance } from '../../../redux/modules/attendance';
 import { Event } from '../../../redux/modules/event';
 import { PracticeItem } from '../../../redux/modules/practice';
-import { Heading } from '../../uiParts/Heading';
-import { Modal } from '../../uiParts/Modal';
-import { SecondaryButton } from '../../uiParts/SecondaryButton';
-import { StringWithUrl } from '../../uiParts/StringWithUrl';
-import styles from './index.module.scss';
+import { PracticeDetail as Presentational } from './PracticeDetail';
 
 type Props = {
   event: Event;
@@ -29,69 +25,24 @@ export const PracticeDetail: React.VFC<Props> = ({
 }) => {
   const { modalRef, handleTogglePracticeModal, handleKeydown } = useModal();
 
+  // TODO: memo化
   const titleDate = getStringDate(event.dates.find((date) => date.id === dateId)?.day);
   const titleDayOfTheWeek = getDayOfTheWeek(event.dates.find((date) => date.id === dateId)?.day);
   const url = practice.locations.find((loc) => loc.dateId === dateId)?.url;
   const content = practice.remarks.find((rmrk) => rmrk.dateId === dateId)?.content;
 
   return (
-    <Modal modalRef={modalRef} handleKeydown={handleKeydown}>
-      <div className={styles.root}>
-        <Heading level={3} label={`${titleDate}${titleDayOfTheWeek}の詳細`} />
-        <dl className={styles.list}>
-          <div className={styles.item}>
-            <dt className={styles.term}>施設1</dt>
-            <dd className={styles.definition}>
-              {practice.locations.find((loc) => loc.dateId === dateId)?.name1 || '未入力'}
-            </dd>
-          </div>
-          <div className={styles.item}>
-            <dt className={styles.term}>施設2</dt>
-            <dd className={styles.definition}>
-              {practice.locations.find((loc) => loc.dateId === dateId)?.name2 || '未入力'}
-            </dd>
-          </div>
-          <div className={styles.item}>
-            <dt className={styles.term}>URL</dt>
-            <dd className={styles.definition}>
-              {url ? (
-                <a href={url} target="_blank" rel="noopener noreferrer">
-                  {url}
-                </a>
-              ) : (
-                '未入力'
-              )}
-            </dd>
-          </div>
-          <div className={styles.item}>
-            <dt className={styles.term}>区分</dt>
-            <dd className={styles.definition}>
-              {practice.plans.find((pln) => pln.dateId === dateId)?.category || '未入力'}
-            </dd>
-          </div>
-          <div className={styles.item}>
-            <dt className={styles.term}>スケジュール</dt>
-            <dd className={styles.definition}>
-              {practice.plans.find((pln) => pln.dateId === dateId)?.schedule || '未入力'}
-            </dd>
-          </div>
-          <div className={styles.item}>
-            <dt className={styles.term}>備考</dt>
-            <dd className={styles.definition}>
-              {content ? <StringWithUrl content={content} /> : '未入力'}
-            </dd>
-          </div>
-          <div className={styles.item}>
-            <dt className={styles.term}>出欠</dt>
-            <dd className={styles.definition}>
-              <div>○：{breakdownAttendances.presence.length}</div>
-              <div>△：{breakdownAttendances.undecided.length}</div>
-              <div>×：{breakdownAttendances.absence.length}</div>
-            </dd>
-          </div>
-        </dl>
-        <SecondaryButton label="閉じる" disabled={false} handleClick={handleTogglePracticeModal} />
-      </div>
-    </Modal>
+    <Presentational
+      practice={practice}
+      dateId={dateId}
+      breakdownAttendances={breakdownAttendances}
+      modalRef={modalRef}
+      handleTogglePracticeModal={handleTogglePracticeModal}
+      handleKeydown={handleKeydown}
+      titleDate={titleDate}
+      titleDayOfTheWeek={titleDayOfTheWeek}
+      url={url}
+      content={content}
+    />
   );
 };
