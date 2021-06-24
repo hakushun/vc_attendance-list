14672adcb3e5f27a985a7ce58dac794768c83561
hakushun <hakushun.pianist@gmail.com>
2021-06-24 16:05:58 +0900

    feat: dialog.tsに他moduleのアクションを接続

--- src/redux/modules/dialog.ts
+++ src/redux/modules/dialog.ts
@@ -2,21 +2,33 @@ import actionCreatorFactory from 'typescript-fsa';
 import { reducerWithInitialState } from 'typescript-fsa-reducers';
 import { createSelector } from 'reselect';
 import { RootState } from './reducers';
+import { resetPassword, signIn, signOut, signUp } from './sign';
+import {
+  create as createAttendance,
+  remove as removeAttendance,
+  update as updateAttendance,
+} from './attendances';
+import { create as createCovid, fetch as fetchCovid } from './covids';
+import { create as createEvent, remove as removeEvent, update as updateEvent } from './events';
+import { update as updatePart } from './parts';
+import { update as updatePractice } from './practice';
+import { update as updateProgram } from './programs';
+import { update as updateRole } from './roles';
 
 export type Message = {
   title: string;
   description: string;
-}
+};
 type Dialog = {
   isOpened: boolean;
   message: Message;
-}
+};
 // action
 const actionCreator = actionCreatorFactory();
 export const toggle = actionCreator<boolean>('TOGGLE_DIALOG');
 
 // initial state
-const INITIAL_STATE: Dialog= {
+const INITIAL_STATE: Dialog = {
   isOpened: false,
   message: {
     title: '',
@@ -29,7 +41,64 @@ const reducer = reducerWithInitialState(INITIAL_STATE)
   .case(toggle, (state, payload) => ({
     ...state,
     isOpened: payload,
-  }));
+  }))
+  .cases(
+    [signUp.async.failed, signIn.async.failed, signOut.async.failed, resetPassword.async.failed],
+    (state, { error }) => ({
+      ...state,
+      isOpened: true,
+      message: {
+        title: error.code,
+        description: error.message,
+      },
+    }),
+  )
+  .cases(
+    [createAttendance.async.failed, updateAttendance.async.failed, removeAttendance.async.failed],
+    (state, { error }) => ({
+      ...state,
+      isOpened: true,
+      message: {
+        title: error.code,
+        description: error.message,
+      },
+    }),
+  )
+  .cases([fetchCovid.async.failed, createCovid.async.failed], (state, { error }) => ({
+    ...state,
+    isOpened: true,
+    message: {
+      title: error.code,
+      description: error.message,
+    },
+  }))
+  .cases(
+    [createEvent.async.failed, updateEvent.async.failed, removeEvent.async.failed],
+    (state, { error }) => ({
+      ...state,
+      isOpened: true,
+      message: {
+        title: error.code,
+        description: error.message,
+      },
+    }),
+  )
+  .cases(
+    [
+      updatePart.async.failed,
+      updatePractice.async.failed,
+      updateProgram.async.failed,
+      updateRole.async.failed,
+    ],
+    (state, { error }) => ({
+      ...state,
+      isOpened: true,
+      message: {
+        title: error.code,
+        description: error.message,
+      },
+    }),
+  );
 export default reducer;
 
 // selector
