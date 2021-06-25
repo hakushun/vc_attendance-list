815c2bde558bb70ff51eb7deb96aa2133671895e
hakushun <hakushun.pianist@gmail.com>
2021-06-25 18:57:40 +0900

    feat: useDialog.ts作成

---
+++ src/hooks/useDialog.ts
@@ -0,0 +1,52 @@
+import { MutableRefObject, useCallback, useEffect, useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { handleTabDown } from '../libs/utils/handleTabDown';
+import {
+  Message,
+  selectDialogIsOpened,
+  selectDialogMessage,
+  toggle,
+} from '../redux/modules/dialog';
+
+type Hooks = {
+  dialogRef: MutableRefObject<HTMLElement | null>;
+  dialogIsOpened: boolean;
+  dialogMessage: Message;
+  handleToggleDialog: () => void;
+  handleKeydown: (_e: React.KeyboardEvent<HTMLElement>) => void;
+};
+export const useDialog = (): Hooks => {
+  const dispatch = useDispatch();
+  const dialogRef = useRef<HTMLElement | null>(null);
+  const dialogIsOpened = useSelector(selectDialogIsOpened);
+  const dialogMessage = useSelector(selectDialogMessage);
+
+  const handleToggleDialog = useCallback(() => {
+    dispatch(toggle(!dialogIsOpened));
+  }, [dialogIsOpened, dispatch]);
+
+  const handleKeydown = useCallback(
+    (e: React.KeyboardEvent<HTMLElement>) => {
+      switch (e.key) {
+        case 'Escape':
+          dispatch(toggle(false));
+          break;
+        case 'Tab':
+          handleTabDown(dialogRef, e);
+          break;
+        default:
+          break;
+      }
+    },
+    [dispatch],
+  );
+
+  useEffect(() => {
+    dialogRef.current?.focus();
+    return () => {
+      // TODO: focus management
+    };
+  }, [dialogIsOpened]);
+
+  return { dialogRef, dialogIsOpened, dialogMessage, handleToggleDialog, handleKeydown };
+};
