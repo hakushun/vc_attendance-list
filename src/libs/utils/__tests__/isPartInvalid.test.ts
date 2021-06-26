b1162bf00ce455010a933e543990f35417f34abc
hakushun <hakushun.pianist@gmail.com>
2021-06-27 08:52:32 +0900

    test: isPartInvalid.test.ts作成

---
+++ src/libs/utils/__tests__/isPartInvalid.test.ts
@@ -0,0 +1,33 @@
+import { Part } from '../../../redux/modules/part';
+import { isPartInvalid } from '../isPartInvalid';
+
+describe('isPartInvalid', () => {
+  it('nameがfalsy', () => {
+    const part: Part[] = [
+      {
+        name: '',
+        id: '01F937G3VMXWPPEV6WN1TJ51QW',
+      },
+      {
+        id: '01F937G3VM7M7HZ9QMRVM02E05',
+        name: 'Ob',
+      },
+    ];
+    const result = true;
+    expect(result).toEqual(isPartInvalid(part));
+  });
+  it('問題ない場合', () => {
+    const part = [
+      {
+        name: 'Fl',
+        id: '01F937G3VMXWPPEV6WN1TJ51QW',
+      },
+      {
+        id: '01F937G3VM7M7HZ9QMRVM02E05',
+        name: 'Ob',
+      },
+    ];
+    const result = false;
+    expect(result).toEqual(isPartInvalid(part));
+  });
+});
