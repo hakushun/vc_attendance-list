40a2e13997c68df85c02eb8216fe9e0d54b3954c
hakushun <hakushun.pianist@gmail.com>
2021-06-27 10:04:07 +0900

    test: tab.test.ts作成

---
+++ src/redux/modules/__tests__/tab.test.ts
@@ -0,0 +1,25 @@
+/* eslint-disable no-undefined */
+import reducer, { change, selectTab } from '../tab';
+import { initialState } from './_initialState';
+
+// reducer
+describe('Reducer: tab', () => {
+  it('Initial state', () => {
+    const result = reducer(undefined, { type: '' });
+    expect(result).toEqual({ setting: 'practice' });
+  });
+
+  it('Action: change', () => {
+    const action = change({ setting: 'role' });
+    const result = reducer(undefined, action);
+    expect(result).toEqual({ setting: 'role' });
+  });
+});
+
+// selector
+describe('Selector: tab', () => {
+  it('selectTab', () => {
+    const result = { setting: 'practice' };
+    expect(result).toEqual(selectTab(initialState));
+  });
+});
