66bb6033c36e019418d62f32a998effb9dcc3011
hakushun <hakushun.pianist@gmail.com>
2021-06-29 19:06:54 +0900

    test: remarks.test.ts作成

---
+++ src/redux/modules/__tests__/remarks.test.ts
@@ -0,0 +1,72 @@
+/* eslint-disable no-undefined */
+import { subscribePractice } from '../practice';
+import reducer, { changeRemark, selectRemarks } from '../remarks';
+import { initialState } from './_initialState';
+
+// reducer
+describe('Reducer: remarks', () => {
+  it('Initial state', () => {
+    const result = reducer(undefined, { type: '' });
+    expect(result).toEqual({ remarks: [] });
+  });
+  it('Action: changeRemark', () => {
+    const state = {
+      remarks: [{ dateId: '01F937GTKXDPW3BD3T6KYZBY9N' }],
+    };
+    const payload = {
+      dateId: '01F937GTKXDPW3BD3T6KYZBY9N',
+      content: 'dummy contents',
+    };
+    const action = changeRemark(payload);
+    const result = reducer(state, action);
+    expect(result).toEqual({
+      remarks: [
+        {
+          dateId: '01F937GTKXDPW3BD3T6KYZBY9N',
+          content: 'dummy contents',
+        },
+      ],
+    });
+  });
+  it('Action :subscribePractice', () => {
+    const payload = {
+      locations: [],
+      plans: [],
+      remarks: [
+        {
+          dateId: '01F937GTKXDPW3BD3T6KYZBY9N',
+          content: 'dummy contents',
+        },
+      ],
+    };
+    const action = subscribePractice(payload);
+    const result = reducer(undefined, action);
+    expect(result).toEqual({
+      remarks: [
+        {
+          dateId: '01F937GTKXDPW3BD3T6KYZBY9N',
+          content: 'dummy contents',
+        },
+      ],
+    });
+  });
+});
+
+// selector
+describe('Selector: remarks', () => {
+  it('selectRemarks', () => {
+    const result = [
+      {
+        content: '譜面台持参',
+        dateId: '01F937G4BYBXW1QRV8BYH1VX9Z',
+      },
+      {
+        dateId: '01F937GTKXDPW3BD3T6KYZBY9N',
+      },
+      {
+        dateId: '01F937GV9SP8WNQFTAYC3K1YSQ',
+      },
+    ];
+    expect(result).toEqual(selectRemarks(initialState));
+  });
+});
