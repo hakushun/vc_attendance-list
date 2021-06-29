b626451793f6e0c5c897702ac0b413a787e1f707
hakushun <hakushun.pianist@gmail.com>
2021-06-29 19:07:01 +0900

    test: role.test.ts作成

---
+++ src/redux/modules/__tests__/role.test.ts
@@ -0,0 +1,105 @@
+/* eslint-disable no-undefined */
+import reducer, { changeRadio, changeRole, selectProgramId, selectRole } from '../role';
+import { subscribeRoles } from '../roles';
+import { initialState } from './_initialState';
+
+// reducer
+describe('Reducer: role', () => {
+  it('Initial state', () => {
+    const result = reducer(undefined, { type: '' });
+    expect(result).toEqual({
+      programId: '',
+      role: [],
+    });
+  });
+  it('Action: changeRadio', () => {
+    const payload = { programId: '1234567' };
+    const action = changeRadio(payload);
+    const result = reducer(undefined, action);
+    expect(result).toEqual({
+      programId: '1234567',
+      role: [],
+    });
+  });
+  it('Action: changeRole', () => {
+    const state = {
+      programId: '',
+      role: [
+        {
+          userId: 'gWGFZShxTVZieKGPDTGELbC8Ser2',
+          '01F937NXKS2YDN1M98MZ9G1JWB': '降り番',
+        },
+      ],
+    };
+    const payload = {
+      userId: 'gWGFZShxTVZieKGPDTGELbC8Ser2',
+      '01F937NK2V8K2W2F6QC9PKJZRP': '乗り番',
+    };
+    const action = changeRole(payload);
+    const result = reducer(state, action);
+    expect(result).toEqual({
+      programId: '',
+      role: [
+        {
+          userId: 'gWGFZShxTVZieKGPDTGELbC8Ser2',
+          '01F937NXKS2YDN1M98MZ9G1JWB': '降り番',
+          '01F937NK2V8K2W2F6QC9PKJZRP': '乗り番',
+        },
+      ],
+    });
+  });
+  it('Action: subscribeRoles', () => {
+    const payload = [
+      {
+        userId: 'gWGFZShxTVZieKGPDTGELbC8Ser2',
+        '01F937NXKS2YDN1M98MZ9G1JWB': '降り番',
+        '01F937NK2V8K2W2F6QC9PKJZRP': '乗り番',
+      },
+      {
+        userId: 'AnT5kMzmdJbgexROAlSoHP0KEC12',
+        '01F937NK2V8K2W2F6QC9PKJZRP': '降り番',
+        '01F937NXKS2YDN1M98MZ9G1JWB': '乗り番',
+      },
+    ];
+    const action = subscribeRoles(payload);
+    const result = reducer(undefined, action);
+    expect(result).toEqual({
+      programId: '',
+      role: [
+        {
+          userId: 'gWGFZShxTVZieKGPDTGELbC8Ser2',
+          '01F937NXKS2YDN1M98MZ9G1JWB': '降り番',
+          '01F937NK2V8K2W2F6QC9PKJZRP': '乗り番',
+        },
+        {
+          userId: 'AnT5kMzmdJbgexROAlSoHP0KEC12',
+          '01F937NK2V8K2W2F6QC9PKJZRP': '降り番',
+          '01F937NXKS2YDN1M98MZ9G1JWB': '乗り番',
+        },
+      ],
+    });
+  });
+});
+
+// selector
+describe('Selector: role', () => {
+  it('selectProgramId', () => {
+    const result = '01F937NXKS2YDN1M98MZ9G1JWB';
+    expect(result).toEqual(selectProgramId(initialState));
+  });
+  it('selectRole', () => {
+    const result = [
+      {
+        userId: 'gWGFZShxTVZieKGPDTGELbC8Ser2',
+        '01F937NXKS2YDN1M98MZ9G1JWB': '降り番',
+        '01F937NK2V8K2W2F6QC9PKJZRP': '乗り番',
+      },
+      {
+        userId: 'AnT5kMzmdJbgexROAlSoHP0KEC12',
+        '01F937NK2V8K2W2F6QC9PKJZRP': '降り番',
+        '01F937NXKS2YDN1M98MZ9G1JWB': '乗り番',
+      },
+    ];
+    expect(result).toEqual(selectRole(initialState));
+  });
+});
