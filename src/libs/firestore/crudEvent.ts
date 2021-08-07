5b37f3226ec99744600091c802464f31b1c81b0c
hakushun <hakushun.pianist@gmail.com>
2021-08-07 17:28:59 +0900

    feat: getCurrentAttendances, generateNewAttendances関数の実装と接続

--- src/libs/firestore/crudEvent.ts
+++ src/libs/firestore/crudEvent.ts
@@ -1,3 +1,4 @@
+import { Attendance, AttendanceItem } from '../../redux/modules/app/attendance';
 import { Event } from '../../redux/modules/app/event';
 import { CreatePayload } from '../../redux/modules/domain/events';
 import { getInstance } from './getInstance';
@@ -18,6 +19,29 @@ export const createEvent = async (event: CreatePayload): Promise<Event> => {
   return fetchEvent(id);
 };
 
+const getCurrentAttendances = async (event: Event) => {
+  const snapshot = await db.collection('attendances').doc(event.id).collection('attendance').get();
+  const result: Attendance[] = [];
+  snapshot.forEach((doc) => {
+    result.push(doc.data() as Attendance);
+  });
+  return result;
+};
+const generateNewAttendances = (event: Event, attendances: Attendance[]) => {
+  const result = attendances.map((attendance) => ({
+    ...attendance,
+    attendances: event.dates.reduce<AttendanceItem[]>((acc, date) => {
+      if (attendance.attendances.some((item) => item.dateId === date.id)) {
+        acc.push(attendance.attendances.find((item) => item.dateId === date.id)!);
+        return acc;
+      }
+      acc.push({ dateId: date.id, attendance: 'presence', remark: '' });
+      return acc;
+    }, []),
+  }));
+  return result;
+};
+
 export const updateEvent = async (event: Event): Promise<Event> => {
   await db.collection('events').doc(event.id).set(event, { merge: true });
   // 追加になった日時に紐づくpracticeを作成
@@ -35,6 +59,20 @@ export const updateEvent = async (event: Event): Promise<Event> => {
         .doc(date.id)
         .set(practice, { merge: true });
     }),
+  );
+  // 追加/削除になっ練習日時に紐づくattendanceを追加/削除
+  // 登録されてる出欠の数が多いと処理の負荷が増大
+  const currentAttendances = await getCurrentAttendances(event);
+  const newAttendances = generateNewAttendances(event, currentAttendances);
+  await Promise.all(
+    newAttendances.map((item) =>
+      db
+        .collection('attendances')
+        .doc(event.id)
+        .collection('attendance')
+        .doc(item.userId)
+        .set(item, { merge: true }),
+    ),
   );
   return fetchEvent(event.id!);
 };
