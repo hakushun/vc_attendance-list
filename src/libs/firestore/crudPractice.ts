d47cfb9f2ba711696d1934f00810edf245ee528e
hakushun <hakushun.pianist@gmail.com>
2021-08-07 11:52:40 +0900

    refactor: createPracticeの修正

--- src/libs/firestore/crudPractice.ts
+++ src/libs/firestore/crudPractice.ts
@@ -5,14 +5,36 @@ import { UpdatePayload } from '../../redux/modules/domain/practice';
 const db = getInstance();
 
 export const createPractice = async (event: Event): Promise<void> => {
-  const practice = {
-    locations: event.dates.map((date) => ({ dateId: date.id })),
-    plans: event.dates.map((date) => ({ dateId: date.id })),
-    remarks: event.dates.map((date) => ({ dateId: date.id })),
-  };
-  await db.collection('practices').doc(event.id).set(practice);
+  Promise.all(
+    event.dates.map((date) => {
+      const project = {
+        location: {
+          name1: '',
+          name2: '',
+          url: '',
+          dateId: date.id,
+        },
+        plan: {
+          category: '',
+          schedule: '',
+          dateId: date.id,
+        },
+        remark: {
+          content: '',
+          dateId: date.id,
+        },
+      };
+      return db
+        .collection('practices')
+        .doc(event.id)
+        .collection('practice')
+        .doc(date.id)
+        .set(project);
+    }),
+  );
 };
 
 export const updatePractice = async ({ event, practice }: UpdatePayload): Promise<void> => {
   await db.collection('practices').doc(event.id).set(practice, { merge: true });
 };
+
