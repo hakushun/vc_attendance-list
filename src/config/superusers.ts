8643e366e1b08bb37d09bbe1222920455c70d5ee
hakushun <hakushun.pianist@gmail.com>
2021-09-26 16:56:07 +0900

    feat: superusers.ts追加

---
+++ src/config/superusers.ts
@@ -0,0 +1,14 @@
+export const superusers = [
+  // 代表
+  '21fs8Qt1vcfJf8BADqBRQRAcZWL2',
+  // セクリ
+  '9wTjGvbl4MfSr5a3xcf9CcSiGqI2',
+  'Pp6wdIRBLKaHtHR7DZdkxDqdVns1',
+  'ffaZ4709DuXanG9k0BAKj99oAkz1',
+  '35ta4JrhuROtiOmsUpPawfVSVQk2',
+  // 会場係
+  'FZTKPQYDHmUO2XroWXBqlpRnF7c2',
+  'FAzishZA9dbk7viEoeYyaYYJ4tE3',
+  // 管理者
+  'gWGFZShxTVZieKGPDTGELbC8Ser2',
+];
