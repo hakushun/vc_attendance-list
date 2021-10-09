c8d9401ad29ce22774815dab75b57b64a0fb8aed
hakushun <hakushun.pianist@gmail.com>
2021-10-09 18:03:44 +0900

    refactor: ModalにCloseButtonを接続

--- src/components/uiParts/Modal/index.tsx
+++ src/components/uiParts/Modal/index.tsx
@@ -1,24 +1,29 @@
 import React from 'react';
+import { CloseButton } from '../CloseButton';
 import { Overlay } from '../Overlay/indes';
 import styles from './index.module.scss';
 
 type Props = {
   modalRef: React.MutableRefObject<HTMLElement | null>;
+  handleToggle: () => void;
   handleKeydown: (_e: React.KeyboardEvent<HTMLElement>) => void;
 };
-export const Modal: React.FC<Props> = React.memo(({ modalRef, handleKeydown, children }) => {
-  return (
-    <Overlay>
-      {/* eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions */}
-      <section
-        role="dialog"
-        aria-modal="true"
-        ref={modalRef}
-        tabIndex={-1}
-        className={styles.root}
-        onKeyDown={handleKeydown}>
-        {children}
-      </section>
-    </Overlay>
-  );
-});
+export const Modal: React.FC<Props> = React.memo(
+  ({ modalRef, handleToggle, handleKeydown, children }) => {
+    return (
+      <Overlay>
+        {/* eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions */}
+        <section
+          role="dialog"
+          aria-modal="true"
+          ref={modalRef}
+          tabIndex={-1}
+          className={styles.root}
+          onKeyDown={handleKeydown}>
+          {children}
+          <CloseButton handleToggle={handleToggle} />
+        </section>
+      </Overlay>
+    );
+  },
+);
