887351c555a534412a946d1b641315dd7ec5a4fc
hakushun <hakushun.pianist@gmail.com>
2021-11-07 14:00:02 +0900

    refactor: HeadingにforwardRefを追加

--- src/components/uiParts/Heading/index.tsx
+++ src/components/uiParts/Heading/index.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import styles from './index.module.scss';
 
 type Props = {
   level: number;
   label: string;
 };
-export const Heading: React.VFC<Props> = React.memo(({ level, label }) => {
-  if (level === 2) {
-    return (
-      <h2 className={styles.root}>
-        <span>{label}</span>
-      </h2>
-    );
-  }
-  if (level === 3) {
-    return (
-      <h3 className={styles.root}>
-        <span>{label}</span>
-      </h3>
-    );
-  }
-  return null;
-});
+export const Heading = React.memo(
+  forwardRef<HTMLHeadingElement, Props>(({ level, label }, ref) => {
+    if (level === 2) {
+      return (
+        <h2 className={styles.root} ref={ref} tabIndex={-1}>
+          <span>{label}</span>
+        </h2>
+      );
+    }
+    if (level === 3) {
+      return (
+        <h3 className={styles.root} ref={ref} tabIndex={-1}>
+          <span>{label}</span>
+        </h3>
+      );
+    }
+    return null;
+  }),
+);
