3a915e83aeadf40da77fc32d6a6dab9558048f35
hakushun <hakushun.pianist@gmail.com>
2023-04-23 10:43:46 +0900

    feat: 出欠テーブルに遅刻・早退・未定を表示するためconvertAttendanceの改修

--- src/libs/utils/__tests__/convertAttendance.test.ts
+++ src/libs/utils/__tests__/convertAttendance.test.ts
@@ -6,8 +6,18 @@ describe('convertAttendance', () => {
     expect(result).toEqual(convertAttendance('presence'));
   });
 
+  it('type: late', () => {
+    const result = '遅刻';
+    expect(result).toEqual(convertAttendance('late'));
+  });
+
+  it('type: leaving_early', () => {
+    const result = '早退';
+    expect(result).toEqual(convertAttendance('leaving_early'));
+  });
+
   it('type: undecided', () => {
-    const result = '△';
+    const result = '未定';
     expect(result).toEqual(convertAttendance('undecided'));
   });
 
