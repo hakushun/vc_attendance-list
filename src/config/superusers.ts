89ec3abfeed5f4ea3f4d65fbadec9bd14fe49277
hakushun <hakushun.pianist@gmail.com>
2025-07-27 21:40:30 +0900

    chore: superusersの更新

--- src/config/superusers.ts
+++ src/config/superusers.ts
@@ -2,10 +2,10 @@ export const superusers = [
   // 代表
   '21fs8Qt1vcfJf8BADqBRQRAcZWL2',
   // セクリ
+  '7x8DC9Hl1mUmrBObGnKteFwk9P73',
   '9wTjGvbl4MfSr5a3xcf9CcSiGqI2',
-  'Pp6wdIRBLKaHtHR7DZdkxDqdVns1',
-  'ffaZ4709DuXanG9k0BAKj99oAkz1',
-  '35ta4JrhuROtiOmsUpPawfVSVQk2',
+  'vZuZC0ATHDZpmiqHFEtUYlhPlTJ2',
+  'cfgIDl1tJRbMDjHPZxNCmnMSR5w2',
   // 弦パトリ
   'ir7YEy7HrtZvisRMMOYyxwBLkSS2',
   'RikBQjQIJkXKXIcUVp9nUXSdJUB3',
